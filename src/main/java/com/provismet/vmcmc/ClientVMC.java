767efe303a07cb7ffa193874fbc9f06ff3a9b3e6
Provismet <17149901+Provismet@users.noreply.github.com>
2023-09-30 18:11:25 +0100

    Add more standard blendshapes.

--- src/main/java/com/provismet/vmcmc/ClientVMC.java
+++ src/main/java/com/provismet/vmcmc/ClientVMC.java
@@ -9,24 +9,19 @@ import org.slf4j.LoggerFactory;
 
 import com.provismet.vmcmc.vmc.CaptureRegistry;
 import com.provismet.vmcmc.vmc.PacketSender;
-import com.provismet.vmcmc.vmc.CaptureRegistry.CaptureType;
 
 public class ClientVMC implements ClientModInitializer {
 	public static final String MODID = "vmc-mc";
     public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
-	private static Identifier identifier (String path) {
+	public static Identifier identifier (String path) {
 		return Identifier.of(MODID, path);
 	}
 
 	@Override
 	public void onInitializeClient () {
 		PacketSender.initPort(39541);
-
-		CaptureRegistry.register(identifier("light"), CaptureType.BLEND_SHAPE, client -> {
-			float light = client.getEntityRenderDispatcher().getLight(client.player, client.getTickDelta());
-			return 15f / light;
-		});
+		CaptureRegistry.registerStandardEvents();
 
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			CaptureRegistry.iterate(client);
--- src/main/java/com/provismet/vmcmc/vmc/CaptureRegistry.java
+++ src/main/java/com/provismet/vmcmc/vmc/CaptureRegistry.java
@@ -3,15 +3,48 @@ package com.provismet.vmcmc.vmc;
 import java.util.HashMap;
 import java.util.function.Function;
 
+import com.provismet.vmcmc.ClientVMC;
+
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.registry.tag.FluidTags;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Pair;
 
 public class CaptureRegistry {
     private static final HashMap<Identifier, Pair<CaptureType, Function<MinecraftClient, Float>>> REGISTRY = new HashMap<>();
 
-    public static void register (Identifier path, CaptureType type, Function<MinecraftClient, Float> callback) {
-        CaptureRegistry.REGISTRY.put(path, new Pair<>(type, callback));
+    public static void register (Identifier identifier, CaptureType type, Function<MinecraftClient, Float> callback) {
+        CaptureRegistry.REGISTRY.put(identifier, new Pair<>(type, callback));
+    }
+
+    public static void register (String path, CaptureType type, Function<MinecraftClient, Float> callback) {
+        CaptureRegistry.register(ClientVMC.identifier(path), type, callback);
+    }
+
+    public static void registerStandardEvents () {
+        CaptureRegistry.register("light", CaptureType.BLEND_SHAPE, client -> {
+			float light = client.getEntityRenderDispatcher().getLight(client.player, client.getTickDelta());
+			return 15f / light;
+		});
+
+        CaptureRegistry.register("relative_health", CaptureType.BLEND_SHAPE, client -> {
+            return client.player.getMaxHealth() / client.player.getHealth();
+        });
+
+        CaptureRegistry.register("hurt", CaptureType.BLEND_SHAPE, client -> {
+            final float DURATION = 5f;
+            float elapsed = client.player.getLastAttackedTime() - client.player.age;
+            if (elapsed > DURATION) return 0f;
+            else return elapsed / DURATION;
+        });
+
+        CaptureRegistry.register("in_water", CaptureType.BLEND_SHAPE, client -> {
+            return (float)client.player.getFluidHeight(FluidTags.WATER);
+        });
+
+        CaptureRegistry.register("in_lava", CaptureType.BLEND_SHAPE, client -> {
+            return (float)client.player.getFluidHeight(FluidTags.LAVA);
+        });
     }
 
     public static void iterate (MinecraftClient client) {
